246d5f76087e0dcf629ba2764e0da6e936efab8e
Mangbo <whdudghks45629@gmail.com>
2023-07-03 16:36:40 +0900

    lv1 연습문제

---
+++ level_1/연습문제/19_시저_암호.js
@@ -0,0 +1,71 @@
+function solution(s, n) {
+  let lower = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let upper = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return s
+    .split("")
+    .map((v) => {
+      if (v === " ") {
+        return " ";
+      } else {
+        let arr = lower.includes(v) ? lower : upper;
+        let i = arr.indexOf(v) + n;
+        if (i >= lower.length) i -= lower.length;
+        return arr[i];
+      }
+    })
+    .join("");
+}
