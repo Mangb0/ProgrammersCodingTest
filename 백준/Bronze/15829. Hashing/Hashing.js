94e6fe41636f3138daf60dd7196eb06dd86c5a19
Mangbo <whdudghks45629@gmail.com>
2023-08-21 16:19:27 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+var input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+};
+console.log(
+  [...input[1]].map((v, i) => obj[v] * 31 ** i).reduce((a, b) => a + b)
+);
