a2a4f9ed9ee907b7923a5bd77f65929146c5dd67
Mangbo <whdudghks45629@gmail.com>
2023-08-27 19:06:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1048 ms, Memory: 162348 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,19 +1,23 @@
 var fs = require("fs");
 var input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const obj = {};
-let answer = "";
+const answer = [];
+const map = new Map();
 
-input[3].split(" ").map((v) => {
-  obj[v] = 0;
-});
-
-input[1].split(" ").map((v) => {
-  if (obj[v] !== undefined) obj[+v]++;
-});
-
-for (let v of input[3].split(" ")) {
-  answer += obj[v] + " ";
+for (let v of input[1].split(" ")) {
+  if (map.has(v)) {
+    map.set(v, map.get(v) + 1);
+  } else {
+    map.set(v, 1);
+  }
 }
 
-console.log(answer.trim());
+for (let v of input[3].split(" ")) {
+  if (map.has(v)) {
+    answer.push(map.get(v));
+  } else {
+    answer.push(0);
+  }
+}
+
+console.log(answer.join(" "));
